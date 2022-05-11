06274226064fb7484d773263531df760f1f97f6f
geanderson062 <geanderson062@outlook.com>
2022-05-11 20:35:23 -0300

    .

---
+++ Readme.md
@@ -0,0 +1,19 @@
+#avaliacao de Dev Mobile.
+
+1- componetizaCao
+2- organizado e estruturado
+3- login e senha 
+4- home com os menus 
+5- nos menus 4 opcoes de menu
+5.1- cadastro
+5.2- edicao do cadastro
+5.3- listagem de cadastro
+5.4- exclusao de cadastro
+
+
+Desenvolver API com PHP para retornar um json
+Banco de dados? 
+
+comando fecht para consumir api
+
+
---
+++ api/edit_script.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+
+<?php
+
+//inscluindo arquivo de conexao
+include "../conexao.php";
+
+//criando vareaveis para editar
+$idADM = $_POST ['idADM'];
+$nomeADM= $_POST ['nomeADM'];
+$cpfADM= $_POST ['cpfADM'];
+$telefoneADM= $_POST ['telefoneADM'];
+$senhaADM= $_POST ['senhaADM'];
+$tipoADM= $_POST ['tipoADM'];
+
+//pegando vareaveis e atualizando os dados inserindo nelas 
+$sql= "UPDATE `administrador` SET `idADM`='$idADM',`cpfADM`='$cpfADM',`nomeADM`='$nomeADM',`telefoneADM`='$telefoneADM',`senhaADM`='$senhaADM',`tipoADM`='$tipoADM' 
+WHERE idADM=$idADM";
+
+//aviso se foi atualizado ou nao 
+if (mysqli_query($conexao , $sql)) {
+
+  echo  "<script>
+      
+  alert('Alterado com sucesso');
+  window.location='dashboard_adm.php';
+ 
+  </script>";
+   
+  //mensagem("$nomeADM, Alterado com sucesso" , 'success');
+ 
+   
+}else {
+
+  echo  "<script>
+      
+    alert('NÃO Alterado com sucesso');
+    window.location='dashboard_CDN.php';
+   
+    </script>";
+    
+   // mensagem("$nomeADM, NÃO Alterado com sucesso" , 'danger');
+    
+}
+
+
+?>
+</body>
+</html>
